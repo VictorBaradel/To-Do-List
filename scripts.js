7155249c7ae7d7e61de1f5a9cd11ea3b31983d05
VictorBaradel <victorbaradel@gmail.com>
2025-02-13 21:02:39 -0300

    adicionado comentários no js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-function adicionarTarefa(){
-    let input = document.querySelector("input").value;
+function adicionarTarefa(){ //função para adicionar tarefa
+    let input = document.querySelector("input").value; //variavel para pegar o valor do input
 
-    let li = document.createElement("li");
-    li.innerHTML = input + '<span onclick="removerTarefa(this)">❌</span>';
+    let li = document.createElement("li"); //variavel para criar uma nova li
+    li.innerHTML = input + '<span onclick="removerTarefa(this)">❌</span>'; //adiciona o valor do input na lista(html) e um span com um "x" para remover a tarefa
 
-    document.querySelector("ul").appendChild(li);
-    
-    document.querySelector("input").value = "";
+    document.querySelector("ul").appendChild(li); //adiciona a li criada na ul
+
+    document.querySelector("input").value = ""; //limpa o input para adicionar uma nova tarefa
 }
 
-function removerTarefa(li){
-    li.parentElement.remove();
+function removerTarefa(li){ //função para remover tarefa
+    li.parentElement.remove(); //remove a tarefa clicada
 }
